c1085caa82fd809a5f6c590c0ef51cf6290598f4
ravi.sharma <ravi.sharma@softsages.com>
2024-02-20 12:03:25 +0530

    Initial Commit

---
+++ src/test/java/com/env/jobportalbackend/JobportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.env.jobportalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
