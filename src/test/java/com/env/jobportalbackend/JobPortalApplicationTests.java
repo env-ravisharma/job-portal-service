98cb2ac72490aff8809f720c6be22b335f5ec3f4
Navneet Sharma <navneet.sharma@zenithgodigital.com>
2024-02-24 19:37:12 +0530

    User services code added

--- src/test/java/com/env/jobportalbackend/JobPortalApplicationTests.java
+++ src/test/java/com/env/jobportalbackend/JobPortalApplicationTests.java
@@ -1,22 +1,19 @@
 package com.env.jobportalbackend;
 
-import com.env.jobportalbackend.model.Job;
-import com.env.jobportalbackend.service.JobService;
+import com.env.jobportalbackend.services.UserTestExecutor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import java.util.List;
 
 @SpringBootTest
 class JobPortalApplicationTests {
 
 	@Autowired
-	JobService jobService;
+	UserTestExecutor userTestExecutor;
 
 	@Test
 	void contextLoads() {
-		List<Job> jobs = jobService.getAllJobs();
-		jobs.forEach(job -> System.out.println(job.getTitle()));
+		userTestExecutor.executeTests();
 	}
 
 }
---
+++ src/test/java/com/env/jobportalbackend/services/UserTestExecutor.java
@@ -0,0 +1,15 @@
+package com.env.jobportalbackend.services;
+
+public class UserTestExecutor {
+
+    public void executeTests()
+    {
+        testRegisterUser();
+    }
+
+    private void testRegisterUser()
+    {
+
+    }
+
+}
