9b44235f828a1aff98573b80af2e66f107066f47
Ravi Sharma <ravikumarsharmaofficial@gmail.com>
2024-02-24 19:44:04 +0530

    User services test cases added

--- src/test/java/com/env/jobportalbackend/services/UserTestExecutor.java
+++ src/test/java/com/env/jobportalbackend/services/UserTestExecutor.java
@@ -1,6 +1,13 @@
 package com.env.jobportalbackend.services;
 
+import com.env.jobportalbackend.user.controller.UserController;
+import com.env.jobportalbackend.user.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserTestExecutor {
+
+    @Autowired
+    UserController userController;
 
     public void executeTests()
     {
@@ -9,7 +16,11 @@ public class UserTestExecutor {
 
     private void testRegisterUser()
     {
-
+        User user = new User();
+        user.setName("Ravi Sharma");
+        user.setPhone("8051697206");
+        user.setEmail("ravisharma@xyz.com");
+        userController.registerUser(user);
     }
 
 }
