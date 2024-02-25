449d66a9da9353281246cceec1125c45afcdddaf
Ravi Sharma <ravikumarsharmaofficial@gmail.com>
2024-02-25 19:56:55 +0530

    Code Refactored

---
+++ src/main/java/com/env/jobPortalService/JobPortalApplication.java
@@ -0,0 +1,13 @@
+package com.env.jobPortalService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(JobPortalApplication.class, args);
+	}
+
+}
