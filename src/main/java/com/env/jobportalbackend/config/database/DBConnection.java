8178fc2cc310eecd1bcbc8d96794d1c530de6ace
Ravi Sharma <ravikumarsharmaofficial@gmail.com>
2024-02-25 16:56:21 +0530

    Code Refactored

--- src/main/java/com/env/jobportalbackend/config/database/DBConnection.java
+++ src/main/java/com/env/jobportalbackend/config/database/DBConnection.java
@@ -9,32 +9,14 @@ public class DBConnection {
     public String userName;
     public String password;
 
+    public DBConnection(String url, String userName, String password) {
+        this.url = url;
+        this.userName = userName;
+        this.password = password;
+    }
+
     public Connection getDBConnection() throws Exception {
         return DriverManager.getConnection(url, userName, password);
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
 }
