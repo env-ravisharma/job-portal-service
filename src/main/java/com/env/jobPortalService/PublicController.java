8923174c60e8bd9c5703f58de73cce440e900331
env-ravisharma <env.ravisharma@gmail.com>
2024-02-27 11:54:34 +0530

    packages refactored

--- src/main/java/com/env/jobPortalService/PublicController.java
+++ src/main/java/com/env/jobPortalService/PublicController.java
@@ -1,5 +1,6 @@
 package com.env.jobPortalService;
 
+import com.env.jobPortalService.registration.model.Applicant;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +10,11 @@ public class PublicController {
     @RequestMapping("/")
     public String homePage(){
         return "index";
+    }
+
+    @RequestMapping("addAlien")
+    public String addAlien(Applicant applicant){
+        return "result";
     }
 
 }
---
+++ src/main/webapp/views/result.jsp
@@ -0,0 +1,6 @@
+<%@page language="java" %>
+<html>
+</body>
+<h2>Result is : ${applicant} <h2>
+</body>
+</html>
